a3cc97356bf925cfe0c0c09e58000071059133e0
stephanyes <stephanoiucciolino@gmail.com>
2023-08-16 18:27:32 -0300

    replaced every console.log & error with pino logger and added pino-http to log the req & res

--- service/service.js
+++ service/service.js
@@ -3,6 +3,7 @@ const { web3, contract, mockERC20Contract, mockERC721Contract, BN, getERC20Decim
 const { CONTRACT_ADDRESS, ERC20, ERC721 } = require("../contracts/contracts");
 const { ethers, keccak256 } = require("ethers");
 const { storeListing, getAllListings, getListingById } = require("../redis/redis")
+const logger = require("../pino/pino")
 const listings = [];
 
 const NftService = {
@@ -11,7 +12,7 @@ const NftService = {
         const arr = await getAllListings();
         return arr;
     } catch (err) {
-        console.error("Failed to retrieve listings:", err);
+        logger.error("Failed to retrieve listings:", err);
         return [];
     }
   },
@@ -21,7 +22,7 @@ const NftService = {
       const listing = await getListingById(listingId)
       return listing
     } catch (err) {
-      console.error(`Failed to retrieve listing with id: ${listingId}`, err);
+      logger.error(`Failed to retrieve listing with id: ${listingId}`, err);
       return [];
     }
   },
@@ -66,7 +67,7 @@ const NftService = {
       const test = await mintTokens(web3, fromAddress, privateKey, toAddress, amount, token );
       return test;
     } catch (error) {
-      console.error("Error:", error);
+      logger.error("Error:", error);
       throw error;
     }
   },
@@ -90,7 +91,7 @@ const NftService = {
   getBalance: async function (address) {
     const tokenContract = mockERC20Contract;
     const balance = await tokenContract.methods.balanceOf(address).call();
-    console.log(`Current balance of ${address}: ${balance}`);
+    logger.info(`Current balance of ${address}: ${balance}`);
   },
   seedListings: async function() {
     const redisListings = await getAllListings();
@@ -98,7 +99,7 @@ const NftService = {
         const exists = listings.some(listing => listing.id === redisListing.id);
         if (!exists) {
             listings.push(redisListing);
-            console.log("Pushing into listings")
+            logger.info("Pushing into listings")
         }
     });
 
@@ -109,52 +110,52 @@ const NftService = {
     const nonceB = await web3.eth.getTransactionCount(bidderAddress);
     try {
       const erc20Decimals = await getERC20Decimals();
-      console.log("111111111111111111111111111")
       const transformedBid = ethers.parseUnits(obj.bid, parseInt(erc20Decimals));
       const transformedTokenId = obj.tokenId;
       const currentGasPrice = await web3.eth.getGasPrice();
       const currentGasPriceBN = new BN(currentGasPrice);
       const adjustedGasPriceBN = currentGasPriceBN.mul(new BN(110)).div(new BN(100));  // increases the gas price by 10%
       const maxGas = 500000;
-      console.log("222222222222222222222222222")
+      logger.info("First gas price calculation done")
       // Estimate Gas for each tx
       const [ gasEstimateERC721, gasEstimateERC20 ] = await Promise.all([ estimateGas( mockERC721Contract, "approve", [CONTRACT_ADDRESS, transformedTokenId], senderAccount), estimateGas( mockERC20Contract, "approve", [CONTRACT_ADDRESS, transformedBid], bidderAddress)]);
       // For ERC20 and ERC721 approve
       const approveABIERC20 = generateApprovalABI(mockERC20Contract, "approve", [CONTRACT_ADDRESS, transformedBid]);
       const approveABIERC721 = generateApprovalABI(mockERC721Contract, "approve", [CONTRACT_ADDRESS, transformedTokenId]);
-      console.log("3333333333333333333333333333333")
+      logger.info("Generated Approval ABI for ERC20 & ERC721 done")
       // Check for users balance before attempting to perform a tx
+      logger.info("Checking balances of both users...")
       await checkBalances(web3, senderAccount, 'seller', ERC20, transformedBid, gasEstimateERC721, adjustedGasPriceBN);
       await checkBalances(web3, bidderAddress, 'bidder', ERC20, transformedBid, gasEstimateERC20, adjustedGasPriceBN);
-      console.log("444444444444444444444444444444")
+      logger.info("Balances done")
       const tx1 = createTxObject({ web3: web3, nonce: nonceA, from: senderAccount, to: ERC721, data: approveABIERC721, gasEstimate: gasEstimateERC721, gasPrice: adjustedGasPriceBN, maxGasLimit: maxGas });
       const tx2 = createTxObject({ web3: web3, nonce: nonceB, from: bidderAddress, to: ERC20, data: approveABIERC20, gasEstimate: gasEstimateERC20, gasPrice: adjustedGasPriceBN, maxGasLimit: maxGas });
       const [signedTx1, signedTx2] = await Promise.all([ signTx(web3, tx1, privateKeyA), signTx(web3, tx2, privateKeyB)]);
-      console.log("55555555555555555555555555555555555")
+      logger.info("First two transactions sign done")
       // Sending the ERC721 and ERC20 approval transactions concurrently
       const [tx1Receipt, tx2Receipt] = await Promise.all([ sendTxAndGetHash(web3, signedTx1), sendTxAndGetHash(web3, signedTx2) ]);
-      console.log("66666666666666666666666666666666")
+      logger.info("Tx1 & Tx2 sent and hash retrieved")
       // Buyer wants NFT
       const marketPlaceAction = generateApprovalABI( contract, "finishAuction",  [ [ obj.collectionAddress, obj.erc20Address, transformedTokenId, transformedBid, ], bidderSig, ownerApprovedSig ]);
-      console.log("77777777777777777777777777777777777")
+      logger.info("Generated FinishAuction ABI done")
       // New nonce needed from sender because of the transaction above
       const nonceD = await web3.eth.getTransactionCount(senderAccount, "pending");
-      console.log("//////////////////////////////")
+      logger.info("Calculating new nonce for sender")
       const currentGasPrice2 = await web3.eth.getGasPrice();
       const currentGasPriceBN2 = new BN(currentGasPrice2);
-      console.log("******************************")
+      logger.info("Second gas price calculation done")
       const adjustedGasPriceBN2 = currentGasPriceBN2.mul(new BN(120)).div(new BN(100));  // increases the gas price by 20%
       const gasEstimateMarketplace = await estimateGas(contract, "finishAuction",  [[ obj.collectionAddress, obj.erc20Address, transformedTokenId, transformedBid ], bidderSig, ownerApprovedSig ], senderAccount)
-      console.log("8888888888888888888888888888")
+      logger.info("Gas estimated for finishAuction done")
       const tx3 = createTxObject({ web3: web3, nonce: nonceD, from: senderAccount, to: CONTRACT_ADDRESS, data: marketPlaceAction, gasEstimate: gasEstimateMarketplace, gasPrice: adjustedGasPriceBN2, maxGasLimit: maxGas });
       const signedTx3 = await signTx(web3, tx3, privateKeyA);
       const tx3Receipt = await sendTxAndGetHash(web3, signedTx3)
       await this.markListingAsSold(obj.tokenId)
-      console.log(`NFT Marketplace ${CONTRACT_ADDRESS} executed finishAuction() successfully`)
-      console.log(`Auction finished between Owner: ${senderAccount} & Buyer: ${bidderAddress}`)
+      logger.info(`NFT Marketplace ${CONTRACT_ADDRESS} executed finishAuction() successfully`)
+      logger.info(`Auction finished between Owner: ${senderAccount} & Buyer: ${bidderAddress}`)
       return { tx1: tx1Receipt, tx2: tx2Receipt, tx3: tx3Receipt };
     } catch (error) {
-      console.error("Error:", error);
+      logger.error("Error:", error);
       throw error;
     }
   },
@@ -163,7 +164,7 @@ const NftService = {
     if (listingIndex !== -1) {
         listings[listingIndex].status = "sold";
         await storeListing(listings[listingIndex]);
-        console.log(`The following token id: ${listings[listingIndex].tokenId} has been sold out`)
+        logger.info(`The following token id: ${listings[listingIndex].tokenId} has been sold out`)
     }
   },
 };
