f59fc7a3bc335e1c023cde92bd9abc7b0bb063ce
stephanyes <stephanoiucciolino@gmail.com>
2023-08-22 23:51:47 -0300

    signingUtils initial testing done

--- test/utils/signingUtils.test.ts
+++ test/utils/signingUtils.test.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Web3 from 'web3';
-import { createTransactionObject, sendSignedTransaction } from '../../utils/index.ts';
+import {
+  createTransactionObject,
+  sendSignedTransaction,
+  sendTransactionAndGetHash,
+  signTransaction,
+  signData,
+} from '../../utils/index.ts';
 import { PinoLogger } from '../../logger/pino.ts';
 
 const { BN } = (Web3.utils as any);
@@ -90,5 +96,150 @@ describe('sendSignedTransaction', () => {
     expect(caughtError).toBeInstanceOf(Error);
     expect(caughtError?.message).toBe('Send failed');
     expect(PinoLogger.error).toHaveBeenCalledWith(`Failed to send signed transaction: ${mockError2.message}`);
+  });
+});
+
+describe('sendTransactionAndGetHash', () => {
+  it('should resolve with hash when transactionHash event is emitted', async () => {
+    // Had to create for each suit this const below
+    // mockSendSignedTransaction because trying
+    // to execute this from outside each it()
+    // was causing some issues
+    const mockSendSignedTransaction: any = {
+      on: jest.fn().mockImplementation(() => mockSendSignedTransaction),
+    };
+
+    const mockWeb3 = {
+      eth: {
+        sendSignedTransaction: jest.fn(() => mockSendSignedTransaction),
+      },
+    } as unknown as Web3;
+
+    const mockHash = 'some-mock-hash';
+    await mockSendSignedTransaction
+      .on.mockImplementation(
+        (event: string, callback: (arg0: string) => void) => {
+          if (event === 'transactionHash') {
+            callback(mockHash);
+          }
+          return mockSendSignedTransaction;
+        },
+      );
+
+    const signedTx = {
+      rawTransaction: 'some-raw-transaction',
+    };
+
+    const result = await sendTransactionAndGetHash(mockWeb3, signedTx);
+
+    expect(result).toBe(mockHash);
+    expect(PinoLogger.info).toHaveBeenCalledWith('Sending Signed Transaction');
+  });
+  it('should reject with error when error event is emitted', async () => {
+    const mockSendSignedTransaction: any = {
+      on: jest.fn().mockImplementation(() => mockSendSignedTransaction),
+    };
+    const mockWeb3 = {
+      eth: {
+        sendSignedTransaction: jest.fn(() => mockSendSignedTransaction),
+      },
+    } as unknown as Web3;
+    const mockError = new Error('Some Error');
+    await mockSendSignedTransaction
+      .on.mockImplementation(
+        (event: string, callback: (arg0: Error) => void) => {
+          if (event === 'error') {
+            callback(mockError);
+          }
+          return mockSendSignedTransaction;
+        },
+      );
+
+    const signedTx = {
+      rawTransaction: 'some-raw-transaction',
+    };
+
+    await expect(sendTransactionAndGetHash(mockWeb3, signedTx)).rejects.toEqual(mockError);
+    expect(PinoLogger.error).toHaveBeenCalledWith(`Failed to get transaction hash: ${mockError.message}`);
+  });
+});
+
+describe('signTransaction', () => {
+  let mockWeb3: any;
+  const mockTransaction = { rawTransaction: 'some-raw-transaction' };
+  const mockPrivateKey = 'private-key';
+
+  beforeEach(() => {
+    mockWeb3 = {
+      eth: {
+        accounts: {
+          signTransaction: jest.fn(),
+        },
+      },
+    };
+    jest.resetAllMocks();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should sign the transaction and return the result', async () => {
+    const mockSignedTx = { some: 'signed-transaction' };
+    mockWeb3.eth.accounts.signTransaction.mockResolvedValue(mockSignedTx);
+
+    const result = await signTransaction(mockWeb3, mockTransaction, mockPrivateKey);
+
+    expect(result).toBe(mockSignedTx);
+    expect(PinoLogger.info).toHaveBeenCalledWith('Signing transaction');
+    expect(mockWeb3.eth.accounts.signTransaction)
+      .toHaveBeenCalledWith(mockTransaction, mockPrivateKey);
+  });
+
+  it('should throw and log an error if signing fails', async () => {
+    const mockErrorNew = new Error('Some signing error');
+    mockWeb3.eth.accounts.signTransaction.mockRejectedValue(mockErrorNew);
+
+    await expect(signTransaction(mockWeb3, mockTransaction, mockPrivateKey))
+      .rejects.toEqual(mockErrorNew);
+    expect(PinoLogger.error).toHaveBeenCalledWith(`Failed to sign transaction: ${mockErrorNew.message}`);
+  });
+});
+
+describe('signData', () => {
+  let mockWeb3: any;
+  const mockData = 'some-data';
+  const mockPrivateKey = 'private-key';
+
+  beforeEach(() => {
+    mockWeb3 = {
+      eth: {
+        accounts: {
+          sign: jest.fn(),
+        },
+      },
+    };
+    jest.resetAllMocks();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should successfully sign the data', async () => {
+    const mockSignedData = { signature: 'mockSignature' };
+    mockWeb3.eth.accounts.sign.mockReturnValue(mockSignedData);
+
+    const result = signData(mockWeb3, mockData, mockPrivateKey);
+    expect(result).toEqual(mockSignedData);
+    expect(PinoLogger.info).toHaveBeenCalledWith('Signing data');
+  });
+
+  it('should throw and log an error if signing fails', () => {
+    const mockError = new Error('Some signing error');
+    mockWeb3.eth.accounts.sign.mockImplementation(() => { throw mockError; });
+
+    expect(() => signData(mockWeb3, mockData, mockPrivateKey)).toThrow(mockError);
+    expect(PinoLogger.error).toHaveBeenCalledWith(`Failed to sign data: ${mockError.message}`);
   });
 });
